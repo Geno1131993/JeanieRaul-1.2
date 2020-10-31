1cadb17ed94d8454911c13395fef54532cb49d0b
Danielle <ukka.ukka.nuka.nuka.appu.wa@gmail.com>
2020-10-30 22:41:07 -0700

    Added style elements

--- public/assets/script/index.js
+++ public/assets/script/index.js
@@ -3,13 +3,15 @@
 $(".tablink").on("click", function(event) {
     let contentID = this.id + "-content";
     let activeID = document.getElementsByClassName("active")[0].id;
-    let active = document.getElementById(activeID);
+    if(contentID != activeID){
+        let active = document.getElementById(activeID);
 
-    $(active).removeClass("active").addClass("inactive").hide();
+        $(active).removeClass("active").addClass("inactive").hide();    
 
 
-    let target = document.getElementById(contentID);
-    $(target).fadeIn(500).addClass("active").removeClass("inactive");
+        let target = document.getElementById(contentID);
+        $(target).fadeIn(500).addClass("active").removeClass("inactive");
+    }
 });
 
 
